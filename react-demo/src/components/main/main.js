986d5fc63e2465a029fef10e6c443238fd12d0bf
Tommy <tommyterry2005@gmail.com>
2022-08-08 15:22:18 -0500

    Changed header into a componenet

--- react-demo/src/components/main/main.js
+++ react-demo/src/components/main/main.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Header from './header';
 
 export default function Main() {
     console.log('working');
     return (
         <Grid container justifyContent="center">
             <Grid container>
-
-                <Grid item xs={12} justifyContent="center">
-                    <Box sx={{height: '133px', width: '80%', backgroundColor: '#00ADB5'}}>
-                        <Typography variant="h1" color="white">Phone Pager System</Typography>
-                    </Box>
-                </Grid>
-
+                <Header/>
             </Grid>
         </Grid>
     );
