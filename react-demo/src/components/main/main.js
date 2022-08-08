cb667caf30bbc08599f7729dc6da4a8fcb3a1762
Tommy <tommyterry2005@gmail.com>
2022-08-08 01:12:57 -0500

    Installed MUI, updated README, and created main javascript file.

---
+++ react-demo/src/components/main/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
+export default function Main() {
+    console.log('working');
+    return (
+        <Paper>Main</Paper>
+    );
+}
--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Main from './components/main/main.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Main/>
   </React.StrictMode>
 );
 
