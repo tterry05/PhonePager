4a91c3a5be6d8a6cb78efefaffa980d7b9f051bb
Tommy <tommyterry2005@gmail.com>
2022-08-08 01:25:42 -0500

    Project Guidelines Established

--- react-demo/src/components/main/main.js
+++ react-demo/src/components/main/main.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Main() {
     console.log('working');
     return (
-        <Paper>Main</Paper>
+        <Grid container justifyContent="center">
+            <Grid container>
+
+                <Grid item xs={12} justifyContent="center">
+                    <Box sx={{height: '133px', width: '80%', backgroundColor: '#00ADB5'}}>
+                        <Typography variant="h1" color="white">Phone Pager System</Typography>
+                    </Box>
+                </Grid>
+
+            </Grid>
+        </Grid>
     );
 }
